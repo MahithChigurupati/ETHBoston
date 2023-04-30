c0089568656eb42881b0c5bdde3a948abadfd66b
Babuaravind Gururaj <gururaj.b@northeastern.edu>
2023-04-29 20:56:03 -0400

    front end change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,16 @@ import React, { Component, useState } from 'react';
 import './App.css';
 
 function App() {
-  function handleSubmit() {
-      alert('Data submitted!');
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formData);
+    setFormData({patientName: '',
+    healthID: '',
+    patientDOB: '',
+    patientEmail: '',
+    patientPhNo: ''});
+    document.getElementById('testFinal').reset();
+    // alert('Data submitted!');
   };
 
   function componentDidMount() {
@@ -19,10 +27,25 @@ function App() {
     };
   }
   const [searchText, setSearchText] = useState("");
+  const [formData, setFormData] = useState({
+    patientName: '',
+    healthID: '',
+    patientDOB: '',
+    patientEmail: '',
+    patientPhNo: ''
+  });
 
   function handleInputChange (event) {
     setSearchText(event.target.value);
   };
+
+  function updateInputValue(evt) {
+    const { name, value } = evt.target;    
+    setFormData(val => ({
+      ...val,
+      [name]: value
+    }));
+  }
 
   function handleFormSubmit (event) {
     event.preventDefault();
@@ -32,7 +55,7 @@ function App() {
 
   //render() {
     return (
-    <form id="testFinal">
+      <>
       <input
       type="text"
       placeholder="Search..."
@@ -40,17 +63,18 @@ function App() {
       onChange={handleInputChange}
       />
       <button type="submit" onClick={handleFormSubmit}>Search</button>
+    <form id="testFinal">
       <div className='form-container'>
         <label htmlFor="test1">Patient Name:</label>
-        <input type="text" id="test1" name="test1" placeholder="Enter Patient Name" />
+        <input type="text" id="test1" name="patientName" onChange={updateInputValue} placeholder="Enter Patient Name" />
         <label htmlFor="test2">Health ID:</label>
-        <input type="text" id="test2" name="test2" placeholder="Enter test 2 value" />
+        <input type="text" id="test2" name="healthID" onChange={updateInputValue} placeholder="Enter Patient Health ID" />
         <label htmlFor="test3">Patient D.O.B:</label>
-        <input type="text" id="test3" name="test3" placeholder="Enter test 3 value" />
+        <input type="text" id="test3" name="patientDOB" onChange={updateInputValue} placeholder="Enter test 3 value" />
         <label htmlFor="test3">Patient Email:</label>
-        <input type="text" id="test3" name="test3" placeholder="Enter test 3 value" />
+        <input type="text" id="test3" name="patientEmail" onChange={updateInputValue} placeholder="Enter test 3 value" />
         <label htmlFor="test3">Patient phone Number:</label>
-        <input type="text" id="test3" name="test3" placeholder="Enter test 3 value" />
+        <input type="text" id="test3" name="patientPhNo" onChange={updateInputValue} placeholder="Enter test 3 value" />
         {/* <label htmlFor="test3">Patient D.O.B:</label>
         <input type="text" id="test3" name="test3" placeholder="Enter test 3 value" /> */}
       </div>
@@ -58,6 +82,7 @@ function App() {
           Submit
       </button>
     </form>
+    </>
     );
   // }
 }
